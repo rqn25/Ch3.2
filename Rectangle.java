5a6107174ee5ab166fdfebcae2344eb6ffed77bf
Nguyen <rqn25@email.vccs.edu>
2016-10-13 18:57:13 -0400

    init

---
+++ Rectangle.java
@@ -0,0 +1,8 @@
+public class Rectangle {
+        public static void main(String [] args) {
+	                double width=10.0, height=3.0;
+			                System.out.println("The area of this rectangle: " + width*height);
+					                System.out.println("The perimeter of this rectangle: " + (2*width+2*height));
+							        }
+								}
+
