76b23bf41b597b8f79f1db645d1ae39a6c70a1f3
rqn25 <rqn25@email.vccs.edu>
2016-10-18 17:44:00 -0400

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,8 +1,7 @@
 public class Rectangle {
         public static void main(String [] args) {
-	                double width=10.0, height=3.0;
-			                System.out.println("The area of this rectangle: " + width*height);
-					                System.out.println("The perimeter of this rectangle: " + (2*width+2*height));
-							        }
-								}
-
+	double width=10.0, height=3.0;
+	System.out.println("The area of this rectangle: " + width*height);
+	System.out.println("The perimeter of this rectangle: " + (2*width+2*height));
+	}
+}
